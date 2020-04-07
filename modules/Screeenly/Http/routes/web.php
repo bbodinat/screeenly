239b4f0528774972fac7f0d36801dfb7eb4069ce
Bertrand <bertrand@lesyetis.com>
2020-04-07 19:50:45 +0200

    Disable some routes

--- modules/Screeenly/Http/routes/web.php
+++ modules/Screeenly/Http/routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
-Route::get('try', 'TryController@index')->name('app.try.index');
-Route::post('try', 'TryController@store')->name('app.try.store');
+// Route::get('try', 'TryController@index')->name('app.try.index');
+// Route::post('try', 'TryController@store')->name('app.try.store');
 
-Route::get('oauth/github/redirect', 'OAuth\GithubController@redirect')->name('oauth.github.redirect');
-Route::get('oauth/github/handle', 'OAuth\GithubController@handle')->name('oauth.github.handle');
+// Route::get('oauth/github/redirect', 'OAuth\GithubController@redirect')->name('oauth.github.redirect');
+// Route::get('oauth/github/handle', 'OAuth\GithubController@handle')->name('oauth.github.handle');
 
 // "Setup" or "Onboarding"?
-Route::get('setup/email/', 'Setup\EmailController@create')->name('setup.email.create');
-Route::post('setup/email', 'Setup\EmailController@store')->name('setup.email.store');
+// Route::get('setup/email/', 'Setup\EmailController@create')->name('setup.email.create');
+// Route::post('setup/email', 'Setup\EmailController@store')->name('setup.email.store');
 
 // Routes which require a logged in user
 Route::group(['middleware' => 'auth'], function () {
